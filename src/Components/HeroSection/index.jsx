e45c4e553657c68388ceda3a1a6ea7665b08804e
svandu <svandu08@gmail.com>
2023-04-26 02:06:11 +0530

    product details completed

--- src/Components/HeroSection/index.jsx
+++ src/Components/HeroSection/index.jsx
@@ -1,12 +1,23 @@
-import shoe from "../../assets/Images/Hero-section-img.png"
+import shoe from "../../assets/Images/shoe5.png"
+import "./herosection.scss"
 
 export default function HeroSection() {
   return (
     <div className="hero-section-container">
-        <img src={shoe}/>
+        <img src={shoe} className="shoe-image"/>
         <div className="product-details">
             <p className="product-name">Adidas Men Trainers</p>
-            <p className="product-price">Rs.14,999</p>
+            <p className="product-para">stylish trendy lightweight canvas casual high peak shoe</p>
+            <div className="product-price"><p className="price">Rs.749/-</p> <span className="mrp">MRP</span> <span className="stroke-price">Rs. 4,599</span> <span className="discount-para">(83% off)</span></div>
+            <p className="size-chart">select size</p>
+            <div className="product-size">
+              <div>5</div>
+              <div className="size">6</div>
+              <div className="size">7</div>
+              <div className="size">8</div>
+              <div className="size">9</div>
+            </div>
+            <button className="shop-now-btn">Shop Now</button>
         </div>
     </div>
   )
