b03a9a2375157afcb39add14c4789ab58bbbde56
svandu <svandu08@gmail.com>
2023-04-25 15:22:39 +0530

    Hamburger add

--- src/Components/HamburgerSection/index.jsx
+++ src/Components/HamburgerSection/index.jsx
@@ -1,34 +1,37 @@
 import { useState } from "react";
-import { ReactComponent as HamburgerIcon } from "../../assets/HamburgerIcon.svg";
-import './hamburger.scss';
+import { ReactComponent as HamburgerIcon } from "../../assets/Icons/HamburgerIcon.svg";
+import "./hamburger.scss";
 
 export default function HamburgerSection() {
 
-  const [active, setActive] = useState(0);
+  const [toggle, setToggle] = useState(true)
 
-  const Hamburger = (e) => {
-    setActive(e);
-  }
+  return (
+    <div className="hamburger-container">
+      <div onClick={() => setToggle(!toggle)} className="hamburger-icon">
+      <HamburgerIcon />
+      </div>
 
-  return ( 
-     <div className="hamburger-container">
-        <div className={active ===1 ? "hamburger-btn" : "hamburger-links active"
-        } onClick={() => {
-          Hamburger(1)
-        }}><HamburgerIcon /></div>
-        <div className="hamburger-btn">
-          <HamburgerIcon/>
-        </div>
-
-        <ul className={active ===1 ? "hamburger-btn" : "hamburger-links active"}  >
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Categories</a></li>
-          <li><a href="#">Orders</a></li>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Login/Signup</a></li>
-        </ul>
+      {toggle && (
+        <ul className="hamburger-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Categories</a>
+        </li>
+        <li>
+          <a href="#">Orders</a>
+        </li>
+        <li>
+          <a href="#">Profile</a>
+        </li>
+        <li>
+          <a href="#">Login/Signup</a>
+        </li>
+      </ul>
+      )}
 
     </div>
-  )
+  );
 }
-
