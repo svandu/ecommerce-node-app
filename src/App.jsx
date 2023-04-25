84b8f43bef8e2fb91091861d530873843b35f2fa
svandu <svandu08@gmail.com>
2023-04-25 15:23:12 +0530

    herosection component add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroSection from "./Components/HeroSection";
 import NavigationBar from "./Components/NavigationBar";
 
 export default function App() {
   return (
     <>
       <NavigationBar />
+      <HeroSection />
     </>
   );
 }
