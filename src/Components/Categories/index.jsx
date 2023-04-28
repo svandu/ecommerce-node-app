173839f78b2e0e9da06aa3f81a5bf07dcb7689b8
svandu <svandu08@gmail.com>
2023-04-28 19:02:27 +0530

    object distructuring don

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,9 +1,37 @@
+import "./categories.scss"
+
 export default function Categories() {
+
+  const categories = [
+    {
+      id: 1,
+      name: "Jackets"
+    },
+    {
+      id: 2,
+      name: "Shoes"
+    },
+    {
+      id: 3,
+      name: "Sneakers"
+    },
+    {
+      id: 4,
+      name: "Men"
+    },
+    {
+      id: 5,
+      name: "Women"
+    }
+  ]
+
   return (
-    <>
-        <ul className="catories-products">
-            <li><a href="#"></a></li>
-        </ul>
-    </>
+    <div className="categories-main-container">
+      {categories.map(({name, id}) => (
+        <div className="product-categories" key={id}>
+            <li>{name}</li>
+        </div>
+      ))}
+    </div>
   )
 }
