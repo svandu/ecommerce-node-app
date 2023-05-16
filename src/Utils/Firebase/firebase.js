999c76bf95b1e0c65aad27ca596769bb673bef84
svandu <svandu08@gmail.com>
2023-05-16 16:56:36 +0530

    firebase add

---
+++ src/Utils/Firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+const firebaseConfig = {
+    apiKey: "AIzaSyBmWxRLTrKyxrDl9bqDeT8m_D2NDdKo3oQ",
+    authDomain: "passion-db.firebaseapp.com",
+    projectId: "passion-db",
+    storageBucket: "passion-db.appspot.com",
+    messagingSenderId: "689280462549",
+    appId: "1:689280462549:web:21cc5ba969320a014c3c5c"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+prompt: "select_acount"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
