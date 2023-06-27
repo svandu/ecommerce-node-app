9f785b5000e75692a79d991faf7ee54f5afbd62f
svandu <svandu08@gmail.com>
2023-06-27 23:23:06 +0530

    product card component create

---
+++ src/Components/ProductCard/index.jsx
@@ -0,0 +1,17 @@
+import "./productcard.scss"
+import Button from "../Button/index"
+
+export default function ProductCard({product}) {
+    const {name, price, imageUrl} = product;
+  return (
+    <div className="product-card-container">
+        <img src={imageUrl} alt={`${name}`}/>
+ 
+        <div className="footer">
+            <span className="name">{name}</span>
+            <span className="price"></span>
+        </div>
+        <Button buttonType='inverted'>Add to cart</Button>
+    </div>
+  )
+}
