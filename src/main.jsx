d18dff88c6fd8f49526807c65a93f0fd8d72b56c
svandu <svandu08@gmail.com>
2023-06-28 00:48:53 +0530

    cart context add

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./Components/Context/index.jsx";
 import { ProductsProvider } from "../src/Components/Context/ProductContext/index.jsx"
+import { CartProvider } from "./Components/Context/CartContext/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
+        <CartProvider >
           <App />
+        </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
