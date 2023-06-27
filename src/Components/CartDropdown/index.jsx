1d7f5d22d626542bb95dd2008e19a480482e2751
svandu <svandu08@gmail.com>
2023-06-28 00:33:33 +0530

    cart drop down component add

---
+++ src/Components/CartDropdown/index.jsx
@@ -0,0 +1,12 @@
+import Button from "../Button/index"
+
+import "./cartdropdown.scss"
+export default function CardDropdown() {
+  return (
+    <div className="cart-dropdown-container">
+        <div className="cart-items">
+            <Button>Go To Checkout</Button>
+        </div>
+    </div>
+  )
+}
