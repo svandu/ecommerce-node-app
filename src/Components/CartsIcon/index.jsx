90c9fdca4ad48d1dfa3a5cde13e50632750456c2
svandu <svandu08@gmail.com>
2023-06-28 00:49:12 +0530

    cart toggle functionality done

--- src/Components/CartsIcon/index.jsx
+++ src/Components/CartsIcon/index.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import "./cartsicon.scss";
 import { ReactComponent as CartIcon } from "../../assets/Icons/CartIcon.svg";
+import { CartContext } from "../Context/CartContext";
 
 export default function CartsIcon() {
+  const { isCartOpen, setIsCartOpen} = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
         <CartIcon className="shopping-icon"/>
         <span className="item-count">0</span>
     </div>
