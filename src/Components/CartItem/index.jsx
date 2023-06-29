f009e92f1d516b98076afcd81bc01e3a0e7c99c9
svandu <svandu08@gmail.com>
2023-06-29 21:45:34 +0530

    cart item component quantiy updated

--- src/Components/CartItem/index.jsx
+++ src/Components/CartItem/index.jsx
@@ -1,12 +1,17 @@
-import "./cartitem.scss"
+import "./cartitem.scss";
 import Button from "../Button";
 
-export default function CartItem({cartItem}) {
-    const { name, quantity } = cartItem;
+export default function CartItem({ cartItem }) {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div>
-        <h2>{name}</h2>
-        <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} X ${price}
+        </span>
+      </div>
     </div>
-  )
+  );
 }
