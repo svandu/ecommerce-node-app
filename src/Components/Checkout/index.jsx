7793a5018331ca3eede6512f240d86a36249bafc
svandu <svandu08@gmail.com>
2023-06-29 22:42:15 +0530

    checkout page styling

---
+++ src/Components/Checkout/index.jsx
@@ -0,0 +1,40 @@
+import "./checkout.scss";
+
+import { useContext } from "react";
+
+import CheckoutItems from "../CheckoutItems";
+import { CartContext } from "../Context/CartContext";
+
+export default function Checkout() {
+  const { cartItems, addItemToCart, removeItemToCart } =
+    useContext(CartContext);
+
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => {
+        const { id, name, quantity } = cartItem;
+        return (
+          <CheckoutItems key={cartItem.id} cartItem={cartItem}/>
+        );
+      })}
+      <span className="total">Total : 0 </span>
+    </div>
+  );
+}
