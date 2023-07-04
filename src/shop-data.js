237985646582fbe9ba0098d42a874192973c053a
svandu <svandu08@gmail.com>
2023-07-05 01:15:44 +0530

    json file changes to js

---
+++ src/shop-data.js
@@ -0,0 +1,96 @@
+const SHOP_DATA = [
+    {
+        title: 'Hats',
+        items: [
+            {
+                id: 1,
+                name: "Brown Brim",
+                imageUrl: "https://i.pinimg.com/564x/c3/26/fb/c326fb50d2a3f995603c3dde84576d84.jpg",
+                price: 25
+            },
+            {
+                id: 2,
+                name: "Blue Beanie",
+                imageUrl: "https://i.pinimg.com/564x/b5/8a/a7/b58aa766a7d53638b01262d5af3da244.jpg",
+                price: 18
+            },
+            {
+                id: 3,
+                name: "Brown Cowboy",
+                imageUrl: "https://i.pinimg.com/564x/e8/23/4b/e8234bda5fb101f898e1a2ec52cbd398.jpg",
+                price: 35
+            },
+            {
+                id: 4,
+                name: "Grey Brim",
+                imageUrl: "https://i.pinimg.com/564x/cd/85/55/cd855564740f917239ba40b9917c3ccb.jpg",
+                price: 25
+            }, 
+            {
+                id: 5,
+                name: "Green Beanie",
+                imageUrl: "https://i.pinimg.com/564x/9d/ed/51/9ded511d278ccb9ed7645639faa970c0.jpg",
+                price: 18
+            },
+            {
+                id: 6,
+                name: "Palm Tree Cap",
+                imageUrl: "https://i.pinimg.com/564x/42/0c/a5/420ca59444278625ac2488b3031793e1.jpg",
+                price: 14
+            },
+            {
+                id: 7,
+                name: "Red Beanie",
+                imageUrl: "https://i.pinimg.com/564x/1a/4c/9d/1a4c9d4c1ee92b49bb51bc98b5507dfe.jpg",
+                price: 18
+            },
+            {
+                id: 8,
+                name: "Wolf Cap",
+                imageUrl: "https://i.pinimg.com/564x/e0/0c/a1/e00ca15551d5f57b5e9cd5ede57b9702.jpg",
+                price: 14
+            },
+            {
+                id: 9,
+                name: "Blue Snapback",
+                imageUrl: "https://i.pinimg.com/564x/c9/f4/56/c9f4567569136956fc5dd1797317b85f.jpg",
+                price: 16
+            }
+        ]
+    }, 
+    {
+        title: 'Jackets',
+        items: [
+            {
+                id: 10,
+                name: "Brown Brim",
+                imageUrl: "https://i.pinimg.com/564x/b4/23/67/b4236791c54c9a485c59434461f0750d.jpg",
+                price: 25
+            },
+            {
+                id: 11,
+                name: "Blue Beanie",
+                imageUrl: "https://i.pinimg.com/564x/7b/1a/73/7b1a734b6926743cfd33d0fe0dd00336.jpg",
+                price: 18
+            },
+            {
+                id: 12,
+                name: "Brown Cowboy",
+                imageUrl: "https://i.pinimg.com/236x/18/07/2c/18072cd09d6b35c57145bfcdda6ba38c.jpg",
+                price: 35
+            },
+            {
+                id: 13,
+                name: "Grey Brim",
+                imageUrl: "https://i.pinimg.com/564x/24/26/16/24261658314d1d77061e301347dd80fa.jpg",
+                price: 25
+            }, 
+            {
+                id: 14,
+                name: "Green Beanie",
+                imageUrl: "https://i.pinimg.com/564x/7d/c4/03/7dc40359cd52efd243173296d86f4595.jpg",
+                price: 18
+            },
+        ] 
+    }
+]
