b7e5e4015930695253a5158bc3d92af1ce75e568
svandu <svandu08@gmail.com>
2023-07-24 22:22:16 +0530

    shop udated

--- src/Components/Shop/index.jsx
+++ src/Components/Shop/index.jsx
@@ -1,23 +1,20 @@
-import { Fragment, useContext } from "react";
+import { useContext } from "react";
 import "./shop.scss";
 
 import { CategoryContext } from "../Context/CategoriesContext/index.jsx";
-import ProductCard from "../ProductCard/index.jsx";
+import CategoryPreview from "../CategoryPreview";
+// import ProductCard from "../ProductCard/index.jsx";
 
 export default function Shop() {
   const { categoriesMap } = useContext(CategoryContext );
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
-            </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products}/>
+        )
+      })}
+    </div>
   );
 }
